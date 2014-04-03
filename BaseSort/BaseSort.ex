ec40de2a1e2444d86664f2906d5c25d513056881
kg8gk <noahdblues@gmail.com>
2014-04-03 21:09:28 +0800

    Added insertion sort
    Signed-off-by: kg8gk <noahdblues@gmail.com>

--- BaseSort/BaseSort.ex
+++ BaseSort/BaseSort.ex
@@ -1,9 +1,10 @@
 defmodule BaseSort do
+
+  # 选择排序
   def selection([]), do: []
   def selection([h|t]), do: selection(t, h, [])
 
   defp selection([], min, rest), do: [min | selection rest]
-
   defp selection([h|t], min, rest) do
     if h < min do
       selection t, h, [min | rest]
@@ -11,5 +12,16 @@ defmodule BaseSort do
       selection t, min, [h | rest]
     end
   end
+
+  # 插入排序
+  def insertion(array), do: List.foldl(array, [], &insert/2)
+  defp insert(key, []), do: [key]
+  defp insert(key, array = [h|t]) do
+    if key < h do
+      [key | array]
+    else
+      [h | insert(key, t)]
+    end
+  end 
 end
 
